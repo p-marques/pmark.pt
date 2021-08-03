3ab3f7185dc7fe2e0f0b4cfa80dd07cb24fb57d8
Pedro Dias Marques <pmark.pt@gmail.com>
2021-08-03 23:51:25 +0100

    Update api key access #2

--- src/app/services/nexus-mods.service.ts
+++ src/app/services/nexus-mods.service.ts
@@ -6,24 +6,22 @@ import { catchError } from 'rxjs/operators';
 
 import { NexusGame } from '../core/nexus-game';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'apikey': <string>process.env.NEXUS_API_KEY
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
 export class NexusModsService {
   private baseUrl = '/api';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'apikey': <string>process.env.NEXUS_API_KEY
+    })
+  };
+
   constructor(private http: HttpClient) { }
 
-
-
   public getGameDetails(gameDomainName: string): Observable<NexusGame> {
-    return this.http.get<NexusGame>(this.baseUrl + 'games/' + gameDomainName, httpOptions)
+    return this.http.get<NexusGame>(this.baseUrl + 'games/' + gameDomainName, this.httpOptions)
       .pipe(
         catchError(this.handleError)
       );
