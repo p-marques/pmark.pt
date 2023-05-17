98fdf4e574a6b46f99d55da42bfe749418651b5d
Pedro Dias Marques <pmark.pt@gmail.com>
2023-05-18 00:04:11 +0100

    Fix issue on /home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,28 +8,27 @@ import { SnackService } from '../services/snack.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   private introLines = [
     'I build web apps.',
-    'I\'m a big fan of Angular.',
+    "I'm a big fan of Angular.",
     'I love video games, and modding them.',
     'I also like to develop desktop apps, using .Net or Python.',
   ];
   private currentIndex: number = 0;
-  private subscription: Subscription;
+  private subscription?: Subscription;
 
   constructor(
     private titleService: Title,
     public colorService: AppColorService,
     public snackService: SnackService
-  ) {
-    this.subscription = interval(5000).subscribe((_) => this.moveToNextIndex());
-  }
+  ) {}
 
   public ngOnInit() {
     this.titleService.setTitle('Pedro Dias Marques');
+    this.subscription = interval(5000).subscribe((_) => this.moveToNextIndex());
   }
 
   public getCurrentLine(): string {
@@ -39,12 +38,13 @@ export class HomeComponent implements OnInit, OnDestroy {
   private moveToNextIndex(): void {
     if (this.currentIndex + 1 === this.introLines.length) {
       this.currentIndex = 0;
+      return;
     }
 
     this.currentIndex++;
   }
 
   public ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.subscription?.unsubscribe();
   }
 }
