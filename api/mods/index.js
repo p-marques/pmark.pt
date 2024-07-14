075c19d43371e1bbf2bbe65ac9d3dc82e727ca6d
Pedro Dias Marques <pmark.pt@gmail.com>
2024-07-14 20:50:43 +0100

    fix: api

---
+++ api/mods/index.js
@@ -0,0 +1,37 @@
+module.exports = function (context, req) {
+  var https = require("https");
+
+  var domainName = context.bindingData.domainName;
+  var modId = context.bindingData.id;
+  var options = {
+    host: "api.nexusmods.com",
+    path: `/v1/games/${domainName}/mods/${modId}`,
+    headers: { apiKey: process.env.NEXUS_API_KEY },
+    method: "GET",
+  };
+
+  var response = "";
+  const request = https.request(options, (res) => {
+    context.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", (d) => {
+      response += d;
+    });
+
+    res.on("end", (d) => {
+      context.res = {
+        status: res.statusCode,
+        body: JSON.parse(response),
+        headers: { "Content-Type": "application/json" },
+      };
+      context.done();
+    });
+  });
+
+  request.on("error", (error) => {
+    context.log.error(error);
+    context.done();
+  });
+
+  request.end();
+};
